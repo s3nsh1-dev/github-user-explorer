c6600ccd86a2ebc542a998b6726fc88a780c9530
s3nsh1-dev <shubham228pandey@gmail.com>
2025-06-23 05:48:52 +0530

    router added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,17 @@ import Home from "./page/Home";
 import Explorer from "./page/Explorer";
 import ProfileInfo from "./page/ProfileInfo";
 import NotFound from "./page/NotFound";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Home />
-      <Explorer />
-      <ProfileInfo />
-      <NotFound />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explorer />} />
+        <Route path="/profile-info" element={<ProfileInfo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 };
