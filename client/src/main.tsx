bee30c81ce3bb15ebc4045b4073ee04bfa2a8a3a
s3nsh1-dev <shubham228pandey@gmail.com>
2025-06-25 18:51:44 +0530

    HomePage done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,10 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
 import "./index.css";
 import ModeContextProvider from "./context/ModeContextProvider.tsx";
 
