ab8312b163c0827aab0e12e29db2b00b43c20dd1
s3nsh1-dev <shubham228pandey@gmail.com>
2025-07-25 22:58:25 +0530

    some understand and helpful comments

--- client/src/hooks/useInfiniteUsers.ts
+++ client/src/hooks/useInfiniteUsers.ts
@@ -8,8 +8,14 @@ const useInfiniteUsers = (query: string) => {
   return useInfiniteQuery<GitHubUserSearchResult>({
     queryKey: ["users", query],
     queryFn: async (context: QueryFunctionContext) => {
+      /* When getNextPageParam returns undefined, React Query stops
+      fetching more pages. so when the first page is being called 
+      context.pageParam is undefined but not undefined which is returned 
+      by getNextPageParam so query starts. But when the total page count
+      is reached then the undefined returned by getNextPageParam makes
+      the react query stop
+      */
       const page = (context.pageParam ?? 1) as number;
-
       const res = await fetch(
         `https://api.github.com/search/users?q=${query}&page=${page}&per_page=16`,
         {
@@ -23,9 +29,13 @@ const useInfiniteUsers = (query: string) => {
 
       return res.json();
     },
-    getNextPageParam: (lastPage, allPages) => {
-      const totalPages = Math.ceil(lastPage.total_count / 10);
-      return allPages.length < totalPages ? allPages.length + 1 : undefined;
+    getNextPageParam: (prevPage, allPages) => {
+      // total_count<count of matched user> is mentioned in every call
+      const totalPages = Math.ceil(prevPage.total_count / 16);
+      // allpages is the array of objects containing the items aka list of names of new users
+      const nextPageNumber =
+        allPages.length < totalPages ? allPages.length + 1 : undefined;
+      return nextPageNumber;
     },
     initialPageParam: 1,
     enabled: !!query,
--- client/src/page/Explorer.tsx
+++ client/src/page/Explorer.tsx
@@ -3,9 +3,22 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import UserCards from "../components/UserCards";
-// import useFetchSearchUsers from "../hooks/useFetchSearchUsers";
-// import type { UserObjectType } from "../constants/common.types";
 import useInfiniteUsers from "../hooks/useInfiniteUsers";
+
+const style1 = { display: "flex", flexDirection: "column", gap: 2 };
+const style2 = {
+  textAlign: "center",
+  margin: "15px 0px 5px 0px",
+  fontFamily: "monospace",
+};
+const style3 = {
+  display: "flex",
+  flexWrap: "wrap",
+  justifyContent: "center",
+  // paddingRight: 10,
+};
+const style4 = { mt: 2, alignSelf: "center", mb: 2 };
+const style5 = { textAlign: "center", mt: 2 };
 
 const Explorer = () => {
   const [searchParams] = useSearchParams();
@@ -14,10 +27,10 @@ const Explorer = () => {
   const {
     data,
     isLoading,
+    error,
     isFetchingNextPage,
     hasNextPage,
     fetchNextPage,
-    error,
   } = useInfiniteUsers(query || "noQueryToSearch");
 
   if (isLoading) return <div>....Loading</div>;
@@ -36,43 +49,26 @@ const Explorer = () => {
       );
     })
   );
-
+  console.log("see last page", data?.pages);
   return (
-    <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-      <Typography
-        sx={{
-          textAlign: "center",
-          margin: "15px 0px 5px 0px",
-          fontFamily: "monospace",
-        }}
-      >
+    <Box sx={style1}>
+      <Typography sx={style2}>
         Matching Results : {data?.pages[0].total_count}
       </Typography>
-      <Box
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          justifyContent: "center",
-          // paddingRight: 10,
-        }}
-      >
-        {renderUserCards}
-      </Box>
+      <Box sx={style3}>{renderUserCards}</Box>
       {hasNextPage && (
         <Button
           variant="contained"
           onClick={() => fetchNextPage()}
           disabled={isFetchingNextPage}
-          sx={{ mt: 2, alignSelf: "center", mb: 2 }}
+          sx={style4}
         >
           {isFetchingNextPage ? "Loading more..." : "Load More"}
         </Button>
       )}
 
       {!hasNextPage && (
-        <Box sx={{ textAlign: "center", mt: 2 }}>
-          🎉 You’ve reached the end of results!
-        </Box>
+        <Box sx={style5}>🎉 You’ve reached the end of results!</Box>
       )}
     </Box>
   );
